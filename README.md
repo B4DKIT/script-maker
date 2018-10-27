f4b6740365871a998d168d43a1bd6b44209517e6
B4DKIT <44515967+B4DKIT@users.noreply.github.com>
2018-10-27 20:12:31 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# script-maker
-Tools buatan B4D KITTY
+Tools ini dibuat tanggal 26 Oktober 2018
+
+B4D KITTY sepenuhnya tidak bertanggung jawab atas penyalahgunaan tools ini 
+
+mohon gunakan dengan bijak:)
+
+
+sampai jumpa di tools berikutnya
---
+++ make.py
@@ -0,0 +1,441 @@
+#!/usr/bin/python
+#######################
+mess ="""{+}=========================================={+}
+               SCRIPT DEFACE MAKER
+                  BY B4D KITTY
+{+}=========================================={+}""" 
+#Open the index
+print mess
+print "{+}Karya Santri Pasuruan{+}"
+print "{+}Coded By B4D KITTY{+}"
+title = raw_input("Judul SC Deface: ")
+alert = raw_input("Pesan Peringatan: ")
+background = raw_input("Background SC Deface: ")
+img = raw_input("Link Gambar Terserah Lu (Posisi di Atas): ")
+marquee = raw_input("Thanks To. Gunakan | untuk memisahkan nama: ")
+pesan = raw_input("Masukan pesan. pakai kode <br> untuk mengganti baris: ")
+bawah = raw_input("Link Gambar terserah Lu (Posisi di Bawah): ")
+font = raw_input("pilih warna tulisan(contoh=red) ")
+copy = raw_input("Coded By :")
+hacked = raw_input("Hacked By: ")
+cepat = raw_input("Kecepatan scroll Nama temen hacker lu(contoh=100): ")
+psnbwh = raw_input("Pesan di bawah gambar terakhir: ")
+
+fo = open("sc-defes.html","w")
+
+messagescript1 ="""<!DOCTYPE html>
+ <head>
+ <style> .ptrk { border-radius: 100%;} body { background	: #000; background-image: url("""
+messagescript2 = background
+messagescript3 ="""); background-repeat:no-repeat; background-attachment: fixed; background-size: 1370px; font-family	: Courier new; text-align	: center; color		: white; } pre { margin-top: 9%; }.error { text-align: center; font-family: 'Gilda Display', serif; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; overflow: default; } .info { text-align: center; width: 200px; height: 60px; margin: auto; position: absolute; top: 280px; bottom: 0; left: 20px; right: 0; -webkit-animation: noise-3 1s linear infinite; animation: noise-3 1s linear infinite; } .info:after { content: 'OWNED'; font-family: OCR-A; font-size: 100px; text-align: center; width: 800px; margin: auto; position: absolute; top: 20px; bottom: 0; left: 40px; right: 0; opacity: 0; color: white; -webkit-animation: noise-1 .2s linear infinite; animation: noise-1 .2s linear infinite; } .wrap { top: 30%; left: 25%; height: 200px; margin-top: -100px; position: absolute; } code { color: white; } span.blue { color: #48beef; } span.comment { color: #7f8c8d; } span.orange { color: #f39c12; } span.green { color: #33cc33; } .viewFull { font-family:OCR-A; color:orange; text-decoration:; } 	 } @media only screen and (min-height: 500px) { .viewFull{ display:none; 	 	} } 	</style> <style> body { background-color: black; text-shadow:5px 5px 14px red; } a:link, a:visited { color: white; text-align: center; text-decoration: none; display: inline-block; } a:hover, a:active { color: maroon; } 	 img{max-width: 100%;max-width: 100%;} 		h1{letter-spacing: -2px;} 		h1,p{max-width:100%;width: 820px;margin: 0 auto;display: block;} .ndivic404animasi{ border-radius:100em; opacity:0.5; border-top:2px solid #cf2031; border-right:2px solid #0f7dc8; border-bottom:2px solid #2eb31a; border-left:2px solid #eab823; width:100%; height:100%; } </style> 
+	 <title>"""
+messagescript4 = title
+messagescript5 ="""</title>
+	 <meta charset="UTF-8"/>
+	 <link rel="stylesheet" href="" type="text/css"/>
+ </head>
+ <script>alert ('"""
+messagescript6 = alert
+messagescript7 ="""')</script>
+	 <body>
+	
+	 <script type="text/javascript" src="http://htmlfreecodes.com/codes/rain.js"></script> 
+	 <body bgcolor="black">
+	<center><img class= "ptrk" src="
+	"""
+messagescript8 = img
+messagescript9 ="""
+" width="450" height="450"</img></center>
+	
+	<span id="sec"><div id="foter" class="foter" style="position: absolute; top: 70px; left: -225px; width: 600px; height: 28px; margin: 0px; padding: 10px; font-size: 24px; text-align: center; color: rgb(255, 255, 255); font-family: &quot;trebuchet ms&quot;, Courier new, courier new, sans-serif; transform: rotate(-45deg); transform-origin: 50% 0px 0px; background-color: rgb(0, 0, 0); border: 1px solid rgb(170, 170, 170); opacity: 0.5; z-index: 1200;">
+<font face="courier new"><marquee color="darkblue" behavior="Flip" scrollamount="50" width="50%" style="width: 50%;">Coded By """
+messagescript10 = copy
+messagescript11 ="""</marquee></font></div></span>
+	
+</body>
+	
+	 <center><script type="text/javascript">
+
+//Define first typing example:
+
+new TypingText(document.getElementById("example1"));
+
+//Define second typing example (use "slashing" cursor at the end):
+
+new TypingText(document.getElementById("example2"), 80, function(i){
+
+var ar = new Array("_"," ","_","_"); return " " + ar[i.length %
+
+ar.length]; });
+
+//Type out examples:
+
+TypingText.runAll();
+
+
+</script>
+
+<style type="text/css">body {cursor:url(""),default}</style>
+
+
+
+<div align="center"><table border="0" width="100%"><tr><td>
+
+<font size="7">
+<b><font face="times"><b><center><SCRIPT>
+
+
+
+farbbibliothek = new Array();
+
+
+
+farbbibliothek[0] = new Array("#FF0000","#FF1100","#FF2200","#FF3300","#FF4400","#FF5500","#FF6600","#FF7700","#FF8800","#FF9900","#FFaa00","#FFbb00","#FFcc00","#FFdd00","#FFee00","#FFff00","#FFee00","#FFdd00","#FFcc00","#FFbb00","#FFaa00","#FF9900","#FF8800","#FF7700","#FF6600","#FF5500","#FF4400","#FF3300","#FF2200","#FF1100");
+
+
+
+farbbibliothek[1] = new Array("#FF0000","#FFFFFF","#FFFFFF","#FF0000");
+
+
+
+farbbibliothek[2] = new Array("#FFFFFF","#FF0000","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF","#FFFFFF");
+
+
+
+farbbibliothek[3] = new Array("#FF0000","#FF4000","#FF8000","#FFC000","#FFFF00","#C0FF00","#80FF00","#40FF00","#00FF00","#00FF40","#00FF80","#00FFC0","#00FFFF","#00C0FF","#0080FF","#0040FF","#0000FF","#4000FF","#8000FF","#C000FF","#FF00FF","#FF00C0","#FF0080","#FF0040");
+
+
+
+farbbibliothek[4] = new Array("#FF0000","#EE0000","#DD0000","#CC0000","#BB0000","#AA0000","#990000","#880000","#770000","#660000","#550000","#440000","#330000","#220000","#110000","#000000","#110000","#220000","#330000","#440000","#550000","#660000","#770000","#880000","#990000","#AA0000","#BB0000","#CC0000","#DD0000","#EE0000");
+
+
+farbbibliothek[5] = new Array("#FF0000","#FF0000","#FF0000","#FFFFFF","#FFFFFF","#FFFFFF");
+
+
+farbbibliothek[6] = new Array("#FF0000","#FDF5E6");
+
+
+
+farben = farbbibliothek[4];
+
+
+
+function farbschrift()
+
+
+
+{
+
+
+
+for(var i=0 ; i<Buchstabe.length; i++)
+
+
+
+{
+
+
+
+document.all["a"+i].style.color=farben[i];
+
+
+
+}
+
+
+
+farbverlauf();
+
+
+
+}
+
+
+
+function string2array(text)
+
+
+
+{
+
+
+
+Buchstabe = new Array();
+
+
+
+while(farben.length<text.length)
+
+
+
+{
+
+
+
+farben = farben.concat(farben);
+
+
+
+}
+
+
+
+k=0;
+
+
+
+while(k<=text.length)
+
+
+
+{
+
+
+
+Buchstabe[k] = text.charAt(k);
+
+
+
+k++;
+
+
+
+}
+
+
+
+}
+
+
+
+function divserzeugen()
+
+
+
+{
+
+
+
+for(var i=0 ; i<Buchstabe.length; i++)
+
+
+
+{
+
+
+
+document.write("<span id='a"+i+"' class='a"+i+"'>"+Buchstabe[i] + "</span>");
+
+
+
+}
+
+
+
+farbschrift();
+
+
+
+}
+
+
+
+var a=1;
+
+
+
+function farbverlauf()
+
+
+
+{
+
+
+
+for(var i=0 ; i<farben.length; i++)
+
+
+
+{
+
+
+
+farben[i-1]=farben[i];
+
+
+
+}
+
+
+
+farben[farben.length-1]=farben[-1];
+
+
+
+
+
+
+
+setTimeout("farbschrift()",30);
+
+
+
+}
+
+
+
+//
+
+
+
+var farbsatz=1;
+
+
+
+function farbtauscher()
+
+
+
+{
+
+
+
+farben = farbbibliothek[farbsatz];
+
+
+
+while(farben.length<text.length)
+
+
+
+{
+
+
+
+farben = farben.concat(farben);
+
+
+
+}
+
+
+
+farbsatz=Math.floor(Math.random()*(farbbibliothek.length-0.0001));
+
+
+
+}
+
+
+
+setInterval("farbtauscher()",5000);
+
+
+
+text ="
+"""
+messagescript12 = hacked
+messagescript13 ="""
+";//h
+
+
+
+string2array(text);
+
+
+
+divserzeugen();
+
+
+
+//document.write(text);
+
+
+
+
+</script><br>
+<font size=6><font color=white>Renungan Untukmu</center>
+	 <center><font size=5><font color="""
+messagescript14 = font
+messagescript15 =""">"""
+messagescript16 = pesan
+messagescript17 ="""</center>
+	<marquee scrollamount="250" direction= "right" behavior= "scroll"/>====================================================================================================</marquee><marquee scrollamount="
+	"""
+messagescript18 = cepat
+messagescript19 ="""
+" direction= "left" behavior= "scroll"/><font size=5><font color="""
+messagescript20 = font
+messagescript21 =""">"""
+messagescript22 = marquee
+messagescript23 ="""</font></marquee><br><marquee scrollamount="
+"""
+messagescript24 = cepat
+messagescript25 ="""
+" direction= "right" behavior= "scroll"/><font size=5><font color="""
+messagescript26 = font
+messagescript27 =""">
+</font></marquee><marquee scrollamount="250" direction= "left" behavior= "scroll"/>====================================================================================================</marquee>
+	 <center><img src="
+	 """
+messagescript28 = bawah
+messagescript29 ="""
+" border="0"></img></center>
+	<center><font size=6><font color=white>[<font color="red">+<font color="white">] """
+messagescript30 = psnbwh
+messagescript31 =""" [<font color="red">+<font color="white">]</center></font></body>"""
+
+
+fo.write(messagescript1)
+fo.write(messagescript2)
+fo.write(messagescript3)
+fo.write(messagescript4)
+fo.write(messagescript5)
+fo.write(messagescript6)
+fo.write(messagescript7)
+fo.write(messagescript8)
+fo.write(messagescript9)
+fo.write(messagescript10)
+fo.write(messagescript11)
+fo.write(messagescript12)
+fo.write(messagescript13)
+fo.write(messagescript14)
+fo.write(messagescript15)
+fo.write(messagescript16)
+fo.write(messagescript17)
+fo.write(messagescript18)
+fo.write(messagescript19)
+fo.write(messagescript20)
+fo.write(messagescript21)
+fo.write(messagescript22)
+fo.write(messagescript23)
+fo.write(messagescript24)
+fo.write(messagescript25)
+fo.write(messagescript26)
+fo.write(messagescript27)
+fo.write(messagescript28)
+fo.write(messagescript29)
+fo.write(messagescript30)
+fo.write(messagescript31)
+
+print "{!}Script Berhasil Di Coding"
+print "{!}Ketik ls untuk melihat hasilnya"
+print "{!}Script bernama sc-defes.html"
+
+
+print "{!}Untuk memindahkan scriptnya ke penyimpanan internal ketik mv -f sc-defes.html /sdcard"
+print "{!}Tapi Sebelum mindahin script deface nya cek dulu bisa buka penyimpanan internal ato engga"
+print "{!}Caranya ketik cd /sdcard terus kalo udah  ketik ls"
+print "{!}Kalo muncul pesan error berarti ga bisa buka penyimpanan internal"
+
+
+print "{!}Solusinya ketik cd terus ketik termux-setup-storage"
+print "{!}Sekarang agan bisa buka penyimpanan internal agan"
+print "{!}Sekian tools dari saya"
+print "{!}Mohon gunakan dengan bijak dan bertanggung jawab"
+
+print "{!}Kami dari B4D KITTY tidak bertanggung jawab atas penyalahgunaan tools ini"
+print "{!}Tunggu tools kami berikutnya!!!!"
+
+
+fo.close()
